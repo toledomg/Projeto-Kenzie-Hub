5e9f2d06185836ce88bfff7d1c5448c5b3dc5a5d
toledomg <toledomg@yahoo.com.br>
2023-02-15 08:10:43 -0300

    refactor: refatorado, RenderList com userContext

--- src/providers/UserTechContext.jsx
+++ src/providers/UserTechContext.jsx
@@ -4,12 +4,38 @@ import { api } from "./../services/api";
 export const UserTechContext = createContext({});
 
 export const UserTechProvider = ({ children }) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState("");
+  const [renderTech, setRenderTech] = useState("");
 
   const token = JSON.parse(localStorage.getItem("@HubKenzieToken"));
 
+  useEffect(() => {
+    async function listUser() {
+      try {
+        const response = await api.get("profile", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        setRenderTech(response.data.techs);
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    listUser();
+  }, []);
+
   return (
-    <UserTechContext.Provider value={(user, setUser)}>
+    <UserTechContext.Provider
+      value={{
+        user,
+        setUser,
+        renderTech,
+        setRenderTech,
+      }}
+    >
       {children}
     </UserTechContext.Provider>
   );
